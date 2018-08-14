1e2b69a2db29cb4086638457203f45190107b7e3
ardinusawan <dewa.putu@ruma.co.id>
2018-08-14 16:51:12 +0700

    https://exercism.io/my/solutions/805a5674750845f29d17177bfedb48fc

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,25 +1,26 @@
 defmodule Bob do
 	def hey(input) do
-		chars = for n <- ?A..?Z, do: << n :: utf8 >>
-		chars_rusia = for n <- ?А..?Я, do: << n :: utf8 >>
     cond do
-			String.upcase(input) == input and (String.contains?(input, chars) or String.contains?(input, chars_rusia))->
-					# If question too: Calm down, I know what I'm doing!
-					if String.at(input, -1) == "?", do: "Calm down, I know what I'm doing!", else: "Whoa, chill out!"
-					# else, "Whoa, chill out!"
-			# If question->
-			String.at(input, -1) == "?" ->
-					# If yell and any string: Calm down, I know what I'm doing!
-					if String.upcase(input) == input and String.contains?(input, chars), do: "Calm down, I know what I'm doing!", else: "Sure."
-					# Else: Sure.
-			# If yell and not question and any string ->
-			String.upcase(input) == input and String.at(input, -1) != "?" and String.contains?(input, chars) ->
-				# Whoa, chill out!
-				"Whoa, chill out!"
-			# If empty ->
-				# "Fine. Be that way!"
-			String.length(String.trim(input)) == 0 -> "Fine. Be that way!"
-			true -> "Whatever."
-    end
+				silence?(input) -> "Fine. Be that way!"
+				question_and_caps?(input) -> "Calm down, I know what I'm doing!"
+				shouting?(input) -> "Whoa, chill out!"
+				question?(input) -> "Sure."
+				true -> "Whatever."
+		end
+ end
+ defp silence?(input) do
+		String.length(String.trim(input)) == 0
+ end
+
+ defp shouting?(input) do
+		String.upcase(input) == input and String.downcase(input) != input
+ end
+
+ defp question_and_caps?(input) do
+		String.at(input, -1) == "?" and String.upcase(input) == input and String.downcase(input) != input
+ end
+
+ defp question?(input) do
+	 String.at(input, -1) == "?"
  end
 end
