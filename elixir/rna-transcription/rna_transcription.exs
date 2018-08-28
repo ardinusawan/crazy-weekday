7f339802331f30e076938cbf2a0f4f281078f0d3
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2018-08-29 01:59:36 +0700

    https://exercism.io/my/solutions/0ea069faa9bc4c1697ac01316a5cda3f

---
+++ elixir/rna-transcription/rna_transcription.exs
@@ -0,0 +1,18 @@
+defmodule RNATranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    mapping = %{'A'=>'U', 'C'=>'G', 'T'=>'A', 'G'=>'C'}
+    translate = fn x -> List.first mapping[[x]] end
+    for char <- dna, do: translate.(char)
+  end
+
+
+end
