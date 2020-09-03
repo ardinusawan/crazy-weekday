8e09c4bc6aa484de1b04bda8395cd951382518eb
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2020-09-03 10:30:09 +0800

    Update linting

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -6,18 +6,19 @@ import (
 
 var maps map[rune]int
 
+// IsIsogram is function to check is word isogram or not
 func IsIsogram(input string) bool {
-    maps = make(map[rune]int)
-    for _, r := range input {
-        var char = unicode.ToUpper(r)
+	maps = make(map[rune]int)
+	for _, r := range input {
+		var char = unicode.ToUpper(r)
 
-        if maps[char] > 0 {
-            return false
-        }
+		if maps[char] > 0 {
+			return false
+		}
 
-        if char >= 65 && char <= 90{
-            maps[char] = maps[char] + 1
-        }
-    }
-    return true
+		if char >= 65 && char <= 90 {
+			maps[char] = maps[char] + 1
+		}
+	}
+	return true
 }
